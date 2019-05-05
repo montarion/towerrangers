a16b7cb8002ac9424af3ee59a99d10a47ae90c8d
montarion <jamirograntsaan@gmail.com>
2019-05-05 11:39:19 +0200

    re-add attackhud.

---
+++ EINDESPRINTS/Sprint 3/attackhud.py
@@ -0,0 +1,20 @@
+from bge import logic, events, render
+from client import networking
+from Tower import Tower
+
+scene = logic.getCurrentScene()
+render.showMouse(True)
+cont = logic.getCurrentController()
+own = cont.owner
+
+mouse = logic.mouse
+mouseClick = logic.KX_INPUT_JUST_ACTIVATED == mouse.events[events.LEFTMOUSE]
+
+if mouseClick:
+    print("spawn got mouse click")
+    #spawn unit
+    msg = {"spawn": {"minion":{"name":"Goblin", "location":"SpawnPointWest"}}}
+    networking.sender(msg)
+
+    # test to hurt base
+    tower.getTower()
