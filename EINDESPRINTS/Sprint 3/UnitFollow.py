e2e70f7f0fd4e019da035943e2ae7258db1237d2
montarion <jamirograntsaan@gmail.com>
2019-05-05 15:20:00 +0200

    enemies can take damage, implemented global dictionary as savestate, shooting is networked

--- EINDESPRINTS/Sprint 3/UnitFollow.py
+++ EINDESPRINTS/Sprint 3/UnitFollow.py
@@ -1,6 +1,6 @@
-import bge ,GameLogic, mathutils, math
+import bge, GameLogic, mathutils, math
 from Enemy import Enemy
-
+from savestate import globaldictionary
 
 scene = bge.logic.getCurrentScene()
 coin = scene.objectsInactive['Coin']
@@ -8,12 +8,15 @@ isDead = False
 
 controller = bge.logic.getCurrentController()
 own = controller.owner
-Enemy().buildEnemy()
 
+
+if globaldictionary['enemybuilt'] == False:
+    Enemy().buildEnemy()
+    globaldictionary['enemybuilt'] = True
+    
 
 # plek voor beslissing nemen
 player = scene.objects['defenderPlayer']
-
 
 # follow stuff
 direction = player.position - own.position
@@ -29,15 +32,17 @@ hit = colsen.hitObject
 try:
     arrow = scene.objects["Arrow"]
     if hit == arrow:
-      isDead = True
+        print("\n\n\nHIT BY AN ARROW TO THE KNEE\n\n\n")
+        # isDead = True
+        Enemy().takeDamage(arrow)
+
 except:
     pass
-if  distance <= -10 or distance >= 10:    
+if distance <= -10 or distance >= 10:
     own.setLinearVelocity(direction * 15, True)
 else:
     own.setLinearVelocity(direction * 0, True)
-    
-    
+
 if isDead:
     print("Coin is spawned")
     own.endObject()
