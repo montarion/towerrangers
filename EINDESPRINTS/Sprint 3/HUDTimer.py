4ef9076ab861bddbffd573c1d769ff6b557f86d9
Timo5S <48393168+Timo5S@users.noreply.github.com>
2019-05-13 14:25:54 +0200

    hudtimer toegevoegd

--- EINDESPRINTS/Sprint 3/HUDTimer.py
+++ EINDESPRINTS/Sprint 3/HUDTimer.py
@@ -3,30 +3,45 @@ scene = bge.logic.getCurrentScene()
 cont = bge.logic.getCurrentController()
 own = cont.owner
 
-own['timeLeft'] = 40
-own['wave'] = 1
-own['newWaveStarted'] = False
-own['waveEnded'] = True
 
-#if wave ended timeleft = 40
-#timeleft --
-#Waveended = False
-#newavestarted = True
-#timeleft = 300 sec
-#timeleft --
-#wave ++
-#Waveended = true
+own['buildWave'] = True
+own['fightWave'] = False
+own['timeLeft'] = 5
+own['buildTime'] = 5
+own['fightTime'] = 10
+own['wave'] = 1
 
 def getSeconds(): 
-
-    if own['timeLeft'] > 0:
+    #own['timeLeft'] = 5
+    if own['buildWave']:
         own['timeLeft'] -= 1 / (60 * 1)
         timeLeftRounded = round(own['timeLeft'], 2)
         scene.objects['TextTimeLeft'].text = "Time Left: " + str(timeLeftRounded)
         scene.objects['TextWave'].text = "Wave: " + str(own['wave'])
-    else:
-        own['wave'] += 1
-        own['timeLeft'] = 5
+        if own['timeLeft'] <= 0:
+            own['buildWave'] = False 
+            own['fightWave'] = True 
+            own['timeLeft'] += own['fightTime']
+    if own['fightWave']:
+        own['timeLeft'] -= 1 / (60 * 1)
+        timeLeftRounded = round(own['timeLeft'], 2)
+        scene.objects['TextTimeLeft'].text = "Time Left: " + str(timeLeftRounded)
+        scene.objects['TextWave'].text = "Wave: " + str(own['wave'])
+        if own['timeLeft'] <= 0:
+            own['wave'] += 1
+            own['buildWave'] = True
+            own['fightWave'] = False
+            own['timeLeft'] += own['buildTime']
+
         
+    #else:
+    #own['wave'] += 1
+    #own['timeLeft'] = 5
+        
+def resetSeconds():
+    cont['timer'] = cont['timeLeft']
+    scene.objects['TextTimeLeft'].text = str(cont['timer'])
+    
+
 
 
